42e82883ab4d8e596ddbfdaf186890a375463fd7
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-07-05 19:54:19 +0100

    Rename AtomTable -> IdentifierTable.

---
+++ include/identifier.h
@@ -0,0 +1,31 @@
+#ifndef __mattflow_identifier_h
+#define __mattflow_identifier_h
+
+namespace mattflow {
+    using IdentifierIdx  = uint64_t;
+    using IdentifierHash = uint64_t;
+
+    class IdentifierTable {
+    public:
+        MATTFLOW_NON_COPYABLE(IdentifierTable)
+
+        static IdentifierTable& get() {
+            static IdentifierTable instance;
+            return instance;
+        }
+
+        IdentifierIdx try_insert(std::string_view atom);
+
+        std::string_view get(IdentifierIdx atom_idx);
+    protected:
+        IdentifierTable() { /* Empty. */
+        }
+    protected:
+        std::unordered_map<IdentifierHash, IdentifierIdx> m_hash_idx_map;
+        std::vector<std::string>                          m_identifiers;
+        std::shared_mutex                                 m_mutex;
+    };
+}  // namespace mattflow
+namespace mf = mattflow;
+
+#endif  // __mattflow_identifier_h
---
+++ src/identifier.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+
+#include "identifier.h"
+
+mf::IdentifierIdx mf::IdentifierTable::try_insert(std::string_view identifier) {
+    std::unique_lock lck(m_mutex);
+
+    IdentifierHash hash = std::hash<std::string_view>{}(identifier);
+
+    if (!m_hash_idx_map.contains(hash)) {
+        m_hash_idx_map[hash] = m_identifiers.size();
+        m_identifiers.emplace_back(std::string(identifier));
+    }
+
+    return m_hash_idx_map[hash];
+}
+
+std::string_view mf::IdentifierTable::get(IdentifierIdx identifier_idx) {
+    std::shared_lock lck(m_mutex);
+
+    if (identifier_idx < m_identifiers.size()) {
+        return std::string_view(m_identifiers[identifier_idx]);
+    }
+    return std::string_view();
+}
