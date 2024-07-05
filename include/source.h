6a64b1ed8c908b42fa1403ff2637ea65c98758d3
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-07-05 19:44:39 +0100

    Introduce a source header, providing FileID and SourceView types.

---
+++ include/source.h
@@ -0,0 +1,20 @@
+#ifndef __mattflow_source_h
+#define __mattflow_source_h
+
+namespace mattflow {
+    using FileID = uint64_t;
+
+    /**
+     * @brief A view over source code, with start and end line + column tracked. file_id
+     * represents the file in which this source is located.
+     */
+    struct SourceView {
+        std::string_view src;
+        FileID           file_id;
+        uint64_t         start_line, end_line;
+        uint64_t         start_column, end_column;
+    };
+}  // namespace mattflow
+namespace mf = mattflow;
+
+#endif  // __mattflow_source_h
