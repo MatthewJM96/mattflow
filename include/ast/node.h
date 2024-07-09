b99bbe21675d05b0d166fff12e6dd67dd3894c34
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-07-09 21:19:27 +0100

    Define a struct of buffers for nodes.

--- include/ast/node.h
+++ include/ast/node.h
@@ -9,4 +9,16 @@
 #include "ast/node/string.h"
 #include "ast/node/variable.h"
 
+namespace mattflow {
+    namespace ast {
+        struct NodeBuffers {
+            std::vector<FunctionNode> functions;
+            std::vector<NumberNode>   numbers;
+            std::vector<OperatorNode> operators;
+            std::vector<VariableNode> variables;
+        };
+    }  // namespace ast
+}  // namespace mattflow
+namespace mfast = mattflow::ast;
+
 #endif  // __mattflow_ast_node_h
