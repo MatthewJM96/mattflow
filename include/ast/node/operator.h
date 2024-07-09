bc5e34d5c3891f56fe6f50c14a97d6ac781f2656
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-07-09 21:19:53 +0100

    Add a useful starting point for defining operator nodes.

--- include/ast/node/operator.h
+++ include/ast/node/operator.h
@@ -2,12 +2,51 @@
 #define __mattflow_ast_node_operator_h
 
 #include "ast/node/node.h"
+#include "lex/token_type.h"
 
 namespace mattflow {
     namespace ast {
-        struct UnaryOperatorNode : public Node { };
+        enum class OperatorKind : int16_t {
+            // Boolean operators.
+            NOT        = static_cast<int16_t>(mflex::TokenType::NOT),
+            EQUALS     = static_cast<int16_t>(mflex::TokenType::EQUALS),
+            NOT_EQUALS = static_cast<int16_t>(mflex::TokenType::NOT_EQUALS),
+            LESS_THAN  = static_cast<int16_t>(mflex::TokenType::LESS_THAN),
+            LESS_THAN_OR_EQUAL_TO
+                = static_cast<int16_t>(mflex::TokenType::LESS_THAN_OR_EQUAL_TO),
+            GREATER_THAN = static_cast<int16_t>(mflex::TokenType::GREATER_THAN),
+            GREATER_THAN_OR_EQUAL_TO
+                = static_cast<int16_t>(mflex::TokenType::GREATER_THAN_OR_EQUAL_TO),
 
-        struct BinaryOperatorNode : public Node { };
+            // Assignment.
+            ASSIGN_VALUE = static_cast<int16_t>(mflex::TokenType::ASSIGN_VALUE),
+
+            // Arithmetic operators.
+            MINUS = static_cast<int16_t>(mflex::TokenType::MINUS),
+            PLUS  = static_cast<int16_t>(mflex::TokenType::PLUS),
+            SLASH = static_cast<int16_t>(mflex::TokenType::SLASH),
+            STAR  = static_cast<int16_t>(mflex::TokenType::STAR)
+        };
+
+        OperatorKind cast_token_to_intrinsic(mflex::TokenType tok) {
+            assert(
+                static_cast<int16_t>(tok) >= static_cast<int16_t>(mflex::TokenType::NOT)
+            );
+            assert(
+                static_cast<int16_t>(tok)
+                <= static_cast<int16_t>(mflex::TokenType::STAR)
+            );
+            assert(
+                static_cast<int16_t>(tok)
+                != static_cast<int16_t>(mflex::TokenType::ASSIGN_TYPE)
+            );
+
+            return static_cast<OperatorKind>(static_cast<int16_t>(tok));
+        }
+
+        struct OperatorNode : public Node {
+            OperatorKind kind;
+        };
     }  // namespace ast
 }  // namespace mattflow
 namespace mfast = mattflow::ast;
