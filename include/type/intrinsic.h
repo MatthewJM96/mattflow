6aefe229c2bd5528b93b2a1488237ed9b72fe217
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-07-09 21:22:01 +0100

    Compare cast_token_to_intrinsic parameter to own type.

--- include/type/intrinsic.h
+++ include/type/intrinsic.h
@@ -24,11 +24,11 @@ namespace mattflow {
         IntrinsicType cast_token_to_intrinsic(mflex::TokenType tok) {
             assert(
                 static_cast<int16_t>(tok)
-                >= static_cast<int16_t>(IntrinsicType{ IntrinsicType::CHAR })
+                >= static_cast<int16_t>(mflex::TokenType::CHAR)
             );
             assert(
                 static_cast<int16_t>(tok)
-                <= static_cast<int16_t>(IntrinsicType{ IntrinsicType::FLOAT64 })
+                <= static_cast<int16_t>(mflex::TokenType::FLOAT64)
             );
 
             return static_cast<IntrinsicType>(static_cast<int16_t>(tok));
