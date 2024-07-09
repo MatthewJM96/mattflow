56df012f9be973541ab0fc6626f3641c7a63d463
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-07-09 21:20:23 +0100

    Add a first-pass definition of variable node.

--- include/ast/node/variable.h
+++ include/ast/node/variable.h
@@ -2,10 +2,14 @@
 #define __mattflow_ast_node_variable_h
 
 #include "ast/node/node.h"
+#include "type.h"
 
 namespace mattflow {
     namespace ast {
-        struct VariableNode : public Node { };
+        struct VariableNode : public Node {
+            mftype::Type  type;
+            IdentifierIdx identifier_idx;
+        };
     }  // namespace ast
 }  // namespace mattflow
 namespace mfast = mattflow::ast;
