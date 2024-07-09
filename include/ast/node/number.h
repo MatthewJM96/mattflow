c59c1a2ce6b5d3e27b1550f014e1e6e260654efd
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-07-09 21:24:11 +0100

    First pass definition of Number node.

--- include/ast/node/number.h
+++ include/ast/node/number.h
@@ -2,10 +2,13 @@
 #define __mattflow_ast_node_number_h
 
 #include "ast/node/node.h"
+#include "number.h"
 
 namespace mattflow {
     namespace ast {
-        struct NumberNode : public Node { };
+        struct NumberNode : public Node {
+            Number value;
+        };
     }  // namespace ast
 }  // namespace mattflow
 namespace mfast = mattflow::ast;
