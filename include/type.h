fd140a2722bdcd2a7a540d91863497f426b52afe
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-07-13 14:10:47 +0100

    Add number type and type type to Type.

--- include/type.h
+++ include/type.h
@@ -1,31 +1,35 @@
-#ifndef __mattflow_type_type_h
-#define __mattflow_type_type_h
+#ifndef __mattflow_type_h
+#define __mattflow_type_h
 
 #include "type/function.h"
-#include "type/intrinsic.h"
+#include "type/number.h"
 #include "type/struct.h"
+#include "type/type.h"
 
 namespace mattflow {
     namespace type {
         enum class TypeKind {
             UNRESOLVED,
-            INTRINSIC,
+            NUMBER,
             STRUCT,
-            FUNCTION
+            FUNCTION,
+            TYPE,
+            NULL
         };
 
         struct Type {
             TypeKind type_kind;
 
             union {
-                FunctionType  function_type;
-                IntrinsicType intrinsic_type;
-                StructType    struct_type;
-                void*         null_type;
+                FunctionType function_type;
+                NumberType   number_type;
+                StructType   struct_type;
+                TypeType     type_type;
+                void*        null_type;
             };
         };
     }  // namespace type
 }  // namespace mattflow
 namespace mftype = mattflow::type;
 
-#endif  // __mattflow_type_type_h
+#endif  // __mattflow_type_h
