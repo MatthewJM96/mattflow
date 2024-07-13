4beb4f3849b95d1e959d920174b3e01e05c24c40
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-07-14 00:29:22 +0100

    Add some beginnings of parse() implementation.

--- include/ast/parse.h
+++ include/ast/parse.h
@@ -6,8 +6,25 @@
 
 namespace mattflow {
     namespace ast {
-        void parse(VALIN const mflex::Tokens& tokens);
-    }
+        // TODO(Matthew): work out what will be useful here, for now put some
+        //                suggestions in as placeholders.
+        enum class ParserExpectsFlag {
+            IDENTIFIER    = 0x000,
+            NUMBER        = 0x001,
+            LEFT_PAREN    = 0x002,
+            RIGHT_PAREN   = 0x004,
+            LEFT_BRACE    = 0x008,
+            RIGHT_BRACE   = 0x010,
+            LEFT_BRACKET  = 0x020,
+            RIGHT_BRACKET = 0x040
+        };
+
+        void parse(
+            VALIN const mflex::Tokens& tokens,
+            VALOUT AST&                ast,
+            VALOUT NodeBuffers&        nodes
+        );
+    }  // namespace ast
 }  // namespace mattflow
 namespace mfast = mattflow::ast;
 
--- src/ast/parse.cpp
+++ src/ast/parse.cpp
@@ -4,6 +4,24 @@
 
 #include "ast/parse.h"
 
-void mfast::parse(VALIN const mflex::Tokens& tokens) {
+void mfast::parse(
+    VALIN const mflex::Tokens& tokens, VALOUT AST& ast, VALOUT NodeBuffers& nodes
+) {
     (void)tokens;
+
+    // Ensure buffers are clear for building fresh AST.
+    ast.clear();
+    nodes.functions.clear();
+    nodes.numbers.clear();
+    nodes.operators.clear();
+    nodes.variables.clear();
+    nodes.nil_node = {};
+
+    // Add top-level expression node for module.
+    auto top_level_vert                   = boost::add_vertex(ast);
+    nodes.vertex_node_map[top_level_vert] = &nodes.nil_node;
+
+    // Iterate tokens, parsing as appropriate.
+    for (auto it = tokens.begin(); it != tokens.end(); ++it) {
+    }
 }
