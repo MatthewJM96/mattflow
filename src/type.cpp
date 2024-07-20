af68fda763493d0468a7ef36dbf94aab6b660c74
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-07-20 22:32:55 +0100

    Update register_type_from_token impl.

--- src/type.cpp
+++ src/type.cpp
@@ -2,13 +2,18 @@
 
 #include "type.h"
 
-mftype::Type* mftype::TypeTable::register_type(mflit::IdentifierIdx identifier) {
-    if (m_ident_type_map.contains(identifier)) {
-        return m_ident_type_map[identifier];
+mftype::Type* mftype::TypeTable::register_type_from_token(const mflex::Token& tok) {
+    if (tok.type != mflex::TokenType::IDENTIFIER) {
+        // TODO(Matthew): implement support for other tokens that can reflect a type.
+        assert(false);
+    }
+
+    if (m_ident_type_map.contains(tok.identifier_idx)) {
+        return m_ident_type_map[tok.identifier_idx];
     }
 
     m_types.emplace_back(Type{});
-    m_ident_type_map[identifier] = &m_types.back();
+    m_ident_type_map[tok.identifier_idx] = &m_types.back();
 
     return &m_types.back();
 }
