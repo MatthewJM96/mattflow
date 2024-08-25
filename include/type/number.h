8708aee13c539bb8bef254262d0954596333428d
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-08-25 20:33:49 +0100

    Move cast_token_to_intrinsic to cpp.

--- include/type/number.h
+++ include/type/number.h
@@ -21,18 +21,7 @@ namespace mattflow {
             FLOAT64 = static_cast<int16_t>(mflex::TokenType::FLOAT64),
         };
 
-        NumberType cast_token_to_intrinsic(mflex::TokenType tok) {
-            assert(
-                static_cast<int16_t>(tok)
-                >= static_cast<int16_t>(mflex::TokenType::CHAR)
-            );
-            assert(
-                static_cast<int16_t>(tok)
-                <= static_cast<int16_t>(mflex::TokenType::FLOAT64)
-            );
-
-            return static_cast<NumberType>(static_cast<int16_t>(tok));
-        }
+        NumberType cast_token_to_intrinsic(mflex::TokenType tok);
     }  // namespace type
 }  // namespace mattflow
 namespace mftype = mattflow::type;
