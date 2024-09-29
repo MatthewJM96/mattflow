21dfcde2708f529da0ee7004f8c834f1f234e2bf
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-09-29 11:51:34 +0100

    data() returns raw pointer from string_view even in MSVC's stupid implementation.

--- src/lex/lexer.cpp
+++ src/lex/lexer.cpp
@@ -55,8 +55,9 @@ void mflex::parse(SourceView source_view, VALOUT Tokens& tokens) {
             //   NOTE: reinterpret_cast needed to satisfy MSVC.
             bool found_match = std::regex_search(
 #if defined(MATTFLOW_COMPILER_MSVC)
-                static_cast<const char*>(remaining_source_view.source.begin()),
-                static_cast<const char*>(remaining_source_view.source.end()),
+                remaining_source_view.source.data(),
+                remaining_source_view.source.data()
+                    + remaining_source_view.source.length(),
 #else   // defined(MATTFLOW_COMPILER_MSVC)
                 remaining_source_view.source.begin(),
                 remaining_source_view.source.end(),
