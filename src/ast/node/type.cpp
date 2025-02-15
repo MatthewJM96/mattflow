6ecf4467fb3d22581841a2717acbc36938254c0b
Matthew Marshall <matthew.marshall090396@gmail.com>
2025-02-15 21:55:48 +0000

    Add types to const static variant defs in Mac.

--- src/ast/node/type.cpp
+++ src/ast/node/type.cpp
@@ -3,18 +3,18 @@
 #include "ast/node/type.h"
 
 #if defined(MATTFLOW_OS_MAC)
-mfast::BoolNode::TYPE    = mftype::PrimitiveType::BOOL;
-mfast::CharNode::TYPE    = mftype::PrimitiveType::CHAR;
-mfast::IntNode::TYPE     = mftype::PrimitiveType::INT;
-mfast::Int8Node::TYPE    = mftype::PrimitiveType::INT8;
-mfast::Int16Node::TYPE   = mftype::PrimitiveType::INT16;
-mfast::Int32Node::TYPE   = mftype::PrimitiveType::INT32;
-mfast::Int64Node::TYPE   = mftype::PrimitiveType::INT64;
-mfast::UIntNode::TYPE    = mftype::PrimitiveType::UINT;
-mfast::UInt8Node::TYPE   = mftype::PrimitiveType::UINT8;
-mfast::UInt16Node::TYPE  = mftype::PrimitiveType::UINT16;
-mfast::UInt32Node::TYPE  = mftype::PrimitiveType::UINT32;
-mfast::UInt64Node::TYPE  = mftype::PrimitiveType::UINT64;
-mfast::Float32Node::TYPE = mftype::PrimitiveType::Float32;
-mfast::Float64Node::TYPE = mftype::PrimitiveType::Float64;
+mftype::Type mfast::BoolNode::TYPE    = mftype::PrimitiveType::BOOL;
+mftype::Type mfast::CharNode::TYPE    = mftype::PrimitiveType::CHAR;
+mftype::Type mfast::IntNode::TYPE     = mftype::PrimitiveType::INT;
+mftype::Type mfast::Int8Node::TYPE    = mftype::PrimitiveType::INT8;
+mftype::Type mfast::Int16Node::TYPE   = mftype::PrimitiveType::INT16;
+mftype::Type mfast::Int32Node::TYPE   = mftype::PrimitiveType::INT32;
+mftype::Type mfast::Int64Node::TYPE   = mftype::PrimitiveType::INT64;
+mftype::Type mfast::UIntNode::TYPE    = mftype::PrimitiveType::UINT;
+mftype::Type mfast::UInt8Node::TYPE   = mftype::PrimitiveType::UINT8;
+mftype::Type mfast::UInt16Node::TYPE  = mftype::PrimitiveType::UINT16;
+mftype::Type mfast::UInt32Node::TYPE  = mftype::PrimitiveType::UINT32;
+mftype::Type mfast::UInt64Node::TYPE  = mftype::PrimitiveType::UINT64;
+mftype::Type mfast::Float32Node::TYPE = mftype::PrimitiveType::Float32;
+mftype::Type mfast::Float64Node::TYPE = mftype::PrimitiveType::Float64;
 #endif
