6a112d89e59126f359033f11af7fa528a0a4c010
Matthew Marshall <matthew.marshall090396@gmail.com>
2025-06-20 23:29:19 +0100

    Update traversal of AST for LLVM IR construction to use topologically sorted iteration.

--- src/backend/llvm.cpp
+++ src/backend/llvm.cpp
@@ -4,17 +4,25 @@
 
 #include "backend/llvm.h"
 
+
+// TODO(Matthew): Can we parellelise this?. Possible ways include:
+//                  subtree-level parallelism, e.g. separating top-level functions
+//                  task-based parellelism by expressing dependencies across nodes
+//                  leaf-node parellelism via work stealing (useful for expression heavy
+//                      functions)
+
 struct LLVM_IR_Converter {
     LLVM_IR_Converter(
-        llvm::LLVMContext* _context, llvm::IRBuilder<>* _builder, llvm::Module* _module
+        llvm::LLVMContext* _context, llvm::IRBuilder<>* _builder, llvm::Module* _module, void** _node_data
     ) :
-        context(_context), builder(_builder), module(_module) {
+        context(_context), builder(_builder), module(_module), node_data(_node_data) {
         // Empty.
     }
 
     llvm::LLVMContext* context;
     llvm::IRBuilder<>* builder;
     llvm::Module*      module;
+    void**             node_data;
 
     template <typename NodeType>
     void operator()(const NodeType&) { }
@@ -29,27 +37,7 @@ void mfbe::convert_module_to_llvm_ir(
 ) {
     (void)var_table;
 
-    // Determine module root vertex and all leaf nodes.
-
-    // size_t module_root = *boost::vertices(ast).first;
-    std::queue<mfast::ASTVertex>                queued_vertices;
-    std::unordered_map<mfast::ASTVertex, void*> processed_vertices;
-    for (auto vertex : boost::make_iterator_range(boost::vertices(ast))) {
-        auto [beg, end] = boost::out_edges(vertex, ast);
-
-        if (end - beg == 0) {
-            queued_vertices.push(vertex);
-        }
-    }
-
-    // TODO(Matthew): Can we parellelise this?
-
-    // Set up module, builder and associated LLVM context.
-
-    llvm::LLVMContext context;
-    llvm::IRBuilder   builder(context);
-    llvm::Module      module("a_module", context);
-
+    ////////////////////////////////////////////////////////////////////////////////////
     // The plan:
     //  Iterate leaf nodes, setting up their respective LLVM data and link to this in
     //  hash map. Iterate the nodes pointing to the leaf nodes and do the same, wherever
@@ -57,58 +45,59 @@ void mfbe::convert_module_to_llvm_ir(
     //  processed onto a queue. Iterate all nodes in the queue and then all nodes
     //  pointed to by nodes processed in the last round in the same way. Recurse.
 
+    ////////////////////////////////////////////////////////////////////////////////////
+    // Set up module, builder and associated LLVM context.
+
+    llvm::LLVMContext context;
+    llvm::IRBuilder   builder(context);
+    llvm::Module      module("a_module", context);
+
+    // User data pointers for specific contents of each node.
+    //   Note that different nodes will need to store different data and we will want
+    //   some nice way of allocating space for this. Likely following the pattern of
+    //   allocation of node data in our own AST structure.
+    //   TODO(Matthew): either make this note happen as described or update it to
+    //                  reflect reality.
+    std::unordered_map<mfast::ASTVertex, void*> processed_node_data;
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // Determine initial number of out-edges of each vertex and queue vertices that
+    // have no out-edges (a.k.a. are leaf nodes).
+
+    std::queue<mfast::ASTVertex> queued_vertices;
+    std::unordered_map<mfast::ASTVertex, uint64_t> unprocessed_out_degrees;
+    for (auto vertex : boost::make_iterator_range(boost::vertices(ast))) {
+        auto [beg, end] = boost::out_edges(vertex, ast);
+
+        unprocessed_out_degrees[vertex] = end - beg;
+
+        if ((end - beg) == 0) {
+            queued_vertices.emplace(vertex);
+        }
+    }
+
     while (!queued_vertices.empty()) {
         // Get next vertex to process and pop it from queue.
         mfast::ASTVertex vertex = queued_vertices.front();
         queued_vertices.pop();
 
-        // Iterate all out edges of the vertex we are processing, if any of the
-        // pointed-to vertices are not in the processed_vertices hashmap, then we need
-        // to add the vertex we are processing back onto the end of the queue - a
-        // necessary bit of IR hasn't been generated yet.
-        bool can_process_vertex = true;
-        for (auto edge : boost::make_iterator_range(boost::out_edges(vertex, ast))) {
-            mfast::ASTVertex linked_to_vertex = boost::target(edge);
-
-            auto it = processed_vertices.find(linked_to_vertex);
-            if (it == processed_vertices.end()) {
-                can_process_vertex = false;
-                queued_vertices.push(vertex);
-            }
-        }
-
-        // If vertex had to be added back onto the queue, then don't process it and test
-        // the next vertex.
-        if (!can_process_vertex) continue;
 
         // Visit node info with the LLVM IR converter.
+        void* processed_data = nullptr;
         std::visit(
-            LLVM_IR_Converter{ &context, &builder, &module },
+            LLVM_IR_Converter{ &context, &builder, &module, &processed_data },
             nodes.get_node_info(vertex)
         );
+        
+        processed_node_data[vertex] = processed_data;
+
+        // Decrement out-edges that are unprocessed for all vertices pointing to this
+        // processed vertex.
+        for (auto edge : boost::make_iterator_range(boost::in_edges(vertex, ast))) {
+            mfast::ASTVertex linked_vertex = boost::source(edge, ast);
+            if (--unprocessed_out_degrees[linked_vertex] == 0) {
+                queued_vertices.push(linked_vertex);
+            }
+        }
     }
-
-    // const auto func_type = llvm::FunctionType::get(builder.getVoidTy(), false);
-    // const auto main_func = llvm::Function::Create(
-    //     func_type, llvm::Function::ExternalLinkage, "main", &module
-    // );
-
-    // const auto entry = llvm::BasicBlock::Create(context, "entrypoint", main_func);
-    // builder.SetInsertPoint(entry);
-
-    // const std::vector<llvm::Type*> puts_args = { builder.getInt8Ty()->getPointerTo()
-    // }; const llvm::ArrayRef           puts_args_ref(puts_args);
-
-    // const auto puts_type
-    //     = llvm::FunctionType::get(builder.getInt32Ty(), puts_args_ref, false);
-    // const auto puts_func = module.getOrInsertFunction("puts", puts_type);
-
-    // auto str = builder.CreateGlobalStringPtr("Hello LLVM!\n");
-
-    // builder.CreateCall(puts_func, { str });
-    // builder.CreateRetVoid();
-
-    // module.dump();
-
-    // std::cout << std::endl;
 }
