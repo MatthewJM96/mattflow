49c3d5b68824e5957a92536fc70d5f1aef9f5133
Matthew Marshall <matthew.marshall090396@gmail.com>
2025-06-21 08:15:34 +0100

    First pass at implementing IR converter for number literals.

--- src/backend/llvm.cpp
+++ src/backend/llvm.cpp
@@ -40,7 +40,32 @@ struct LLVM_IR_Converter {
     template <typename NodeType>
     void operator()(const NodeType&) { }
 
-    void operator()(const mfast::NumberValNode&) { }
+    void operator()(const mfast::NumberValNode& node) {
+        // NOTE: For now we just make everything highest precision we support, this
+        //       might not really be sufficient and we might need to do implicit and
+        //       explicit casting of literals before representing the literal in LLVM
+        //       IR.
+
+        // Create an LLVM constant based on the number type (integer or floating-point)
+        llvm::Constant* constant_value = nullptr;
+
+        if (node.value.is_floating_point()) {
+            // Handle floating-point numbers
+            constant_value = llvm::ConstantFP::get(
+                *context, llvm::APFloat(node.value.template as<double>())
+            );
+        } else {
+            // Handle integers
+            constant_value = llvm::ConstantInt::get(
+                llvm::IntegerType::get(*context, 64), node.value.template as<uint64_t>()
+            );
+        }
+
+        // Store the generated value in node_data for potential future use
+        if (node_data) {
+            *node_data = reinterpret_cast<void*>(constant_value);
+        }
+    }
 };
 
 void mfbe::convert_module_to_llvm_ir(
