7c14586c0729195f732793f6c2a18475b131d92e
Matthew Marshall <matthew.marshall090396@gmail.com>
2025-06-30 00:54:03 +0100

    Fix logic of AssignTypeVisitor.

--- include/ast/visitor/type.h
+++ include/ast/visitor/type.h
@@ -25,9 +25,9 @@ namespace mattflow {
             template <typename NodeType>
             void operator()(const NodeType& target) {
                 if constexpr (std::is_base_of_v<IdentifierNode, NodeType>) {
-                    var_table.associate_type(target->name, NodeType::TYPE);
+                    var_table.associate_type(scope, target.name, TypeNodeType::TYPE);
                 } else if constexpr (std::is_base_of_v<ParenExprNode, NodeType>) {
-                    //
+                    // TODO(Matthew): do we want to do anything here?
                 }
             }
         };
