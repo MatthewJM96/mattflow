04c73e1a2b44e5d9a7b6581447246cb56d102a61
Matthew Marshall <matthew.marshall090396@gmail.com>
2025-06-30 09:29:00 +0100

    Don't need mflit:: to access IdentifierIdx in def of INVALID_IDENTIFIER.

--- include/literal/identifier.h
+++ include/literal/identifier.h
@@ -6,7 +6,7 @@ namespace mattflow {
         using IdentifierIdx  = uint64_t;
         using IdentifierHash = uint64_t;
 
-        const IdentifierIdx INVALID_IDENTIFIER = static_cast<mflit::IdentifierIdx>(-1);
+        const IdentifierIdx INVALID_IDENTIFIER = static_cast<IdentifierIdx>(-1);
 
         class IdentifierTable {
         public:
