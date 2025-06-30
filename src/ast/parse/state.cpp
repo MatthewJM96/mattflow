36b9aa4c6d9e657fcc2813f1d9d1410a600c8087
Matthew Marshall <matthew.marshall090396@gmail.com>
2025-06-30 23:45:34 +0100

    Make sure add a scope for the root node (aka global scope).

--- src/ast/parse/state.cpp
+++ src/ast/parse/state.cpp
@@ -42,10 +42,15 @@ void mfast::push_enclosure(
     if ((enclosing_category & NodeProps::ROOT) != NodeProps::ROOT) {
         parser_state.non_operating_vertices.back().emplace_back(enclosing_vertex);
         parser_state.last_seen.back() = NodeProps::NONOP;
-    }
 
+        mfvar::Scope root_scope = boost::add_vertex(scope_tree);
+        parser_state.scopes.emplace_back(root_scope);
+    }
     // If enclosure represents a scope then add a scope to the stack.
-    if ((enclosing_category & NodeProps::SCOPE) == NodeProps::SCOPE) {
+    //  NOTE: We expect that ROOT will not be tagged as a SCOPE so the else isn't
+    //        strictly necessary but it is more robust to assume it could be the case.
+    else if ((enclosing_category & NodeProps::SCOPE) == NodeProps::SCOPE)
+    {
         mfvar::Scope old_scope = parser_state.scopes.back();
         mfvar::Scope new_scope = boost::add_vertex(scope_tree);
 
